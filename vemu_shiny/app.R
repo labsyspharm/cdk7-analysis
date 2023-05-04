021198f6e5a7278370a66973fc35789cfdab1c59
Clemens Hug <clemens.hug@gmail.com>
2023-05-03 21:45:06 -0400

    Add differential expression plot

--- vemu_shiny/app.R
+++ vemu_shiny/app.R
@@ -6,8 +6,10 @@ library(here)
 library(qs)
 library(ggbeeswarm)
 
+theme_set(theme_minimal())
+
 vemu_treated_vs_untreated <- qread(
-  here("deseq", "deseq_res_vemu_treated_vs_untreated.qs")
+  here("data", "deseq_res_vemu_treated_vs_untreated.qs")
 ) %>%
   mutate(
     plot_output_name = paste0(
@@ -21,6 +23,16 @@ vemu_treated_vs_untreated <- qread(
   arrange(
     cell_line, concentration_fbs
   )
+
+vemu_treated_vs_untreated_long <- vemu_treated_vs_untreated %>%
+  mutate(
+    condition_print = paste0(
+      cell_line,
+      " ",
+      concentration_fbs
+    )
+  ) %>%
+  unnest(de_res)
 
 gene_meta <- vemu_treated_vs_untreated$de_res[[1]] %>%
   distinct(ensembl_gene_id, hgnc_symbol) %>%
@@ -40,27 +52,69 @@ vemu_treated_vs_untreated_wide <- vemu_treated_vs_untreated %>%
   mutate(
     neglog10padj = -log10(if_else(padj == 0, .5 * min(padj[padj > 0]), padj))
   ) %>%
-  select(condition, ensembl_gene_id, hgnc_symbol, log2FoldChange, neglog10padj) %>%
-  pivot_wider(
-    names_from = condition,
-    values_from = c(log2FoldChange, neglog10padj)
-  ) %>%
+  select(condition, ensembl_gene_id, hgnc_symbol, log2FoldChange, neglog10padj, padj) %>%
   inner_join(
     gene_meta,
     by = "ensembl_gene_id"
+  ) %>%
+  mutate(
+      hovertext = paste0(
+      "<i>Gene:</i> ", gene_name, "<br>",
+      "<i>Log2 fold change:</i> ", signif(log2FoldChange, 2), "<br>",
+      "<i>Adjusted p-value:</i> ", signif(padj, 2)
+    )
+  ) %>%
+  pivot_wider(
+    names_from = condition,
+    values_from = c(log2FoldChange, neglog10padj, padj, hovertext)
   )
 
+
+
 normalized_counts <- read_csv(
-  here("qc", "normalized_counts.csv.gz")
+  here("data", "normalized_counts.csv.gz")
 )
 
 vst_counts <- read_csv(
-  here("qc", "counts_vst.csv.gz")
+  here("data", "counts_vst.csv.gz")
 )
 
 de_meta_vemu <- read_csv(
-  here("deseq", "de_meta_vemu.csv")
+  here("data", "de_meta_vemu.csv")
 )
+
+filter_by_gene <- function(data, gene_symbol) {
+  filtered <- data %>%
+    filter(hgnc_symbol == gene_symbol)
+  if (nrow(filtered) == 0)
+    # Try gene id instead
+    filtered <- data %>%
+      filter(ensembl_gene_id == gene_symbol)
+  if (nrow(filtered) == 0)
+    stop("Gene not found")
+  filtered
+}
+
+plot_de_single_gene_vemu <- function(gene_symbol) {
+  filtered <- filter_by_gene(vemu_treated_vs_untreated_long, gene_symbol)
+  p <- ggplot(
+    filtered,
+    aes(
+      concentration_fbs, log2FoldChange,
+      color = cell_line,
+      shape = padj < 0.05,
+      group = cell_line
+    )
+  ) +
+    geom_point() +
+    scale_shape_manual(
+      values = c(`TRUE` = 16, `FALSE` = 4)
+    ) +
+    geom_line() +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    labs(shape = "padj < 0.05")
+  p
+}
 
 plot_expression_single_gene_vemu <- function(gene_symbol, counts = c("normalized", "vst")) {
   counts <- match.arg(counts)
@@ -69,13 +123,8 @@ plot_expression_single_gene_vemu <- function(gene_symbol, counts = c("normalized
     vst = vst_counts,
     normalized = normalized_counts
   )
-  df <- counts_table %>%
-    filter(hgnc_symbol == gene_symbol)
-  if (nrow(df) == 0)
-    # Try gene id instead
-    df <- counts_table %>%
-      filter(ensembl_gene_id == gene_symbol)
-   p <- df %>%
+  df <- filter_by_gene(counts_table, gene_symbol)
+  p <- df %>%
     pivot_longer(
       -c(ensembl_gene_id, hgnc_symbol), names_to = "well", values_to = "normalized_count"
     ) %>%
@@ -107,20 +156,21 @@ plot_expression_single_gene_vemu <- function(gene_symbol, counts = c("normalized
    p
 }
 
-
-
 make_volcano_plot <- function(data, condition, title) {
   plot_ly(
     data,
     x = reformulate(paste0("log2FoldChange_", condition)),
     y = reformulate(paste0("neglog10padj_", condition)),
     customdata = ~ensembl_gene_id,
-    hovertext = ~gene_name,
+    hovertext = reformulate(paste0("hovertext_", condition)),
     hoverinfo = "text",
+    # hovertemplate = paste(
+    #   "<i>Gene:</i> %{gene_name}<br>",
+    #   "<i>log2 fold change:</i> %{x}<br>",
+    #   "<i>adjusted p-value:</i> %{text}"
+    # ),
     type = "scatter",
-    mode = "markers",
-    # make the points a bit transparent
-    opacity = 0.5
+    mode = "markers"
   ) %>%
     layout(
       annotations = list(
@@ -152,7 +202,16 @@ ui <- fluidPage(
       )
     )
   ),
-  plotOutput("plot_out_expression_bars")
+  fluidRow(
+    column(
+      width = 8,
+      plotOutput("plot_out_expression_bars")
+    ),
+    column(
+      width = 4,
+      plotOutput("plot_out_de")
+    )
+  )
 )
 
 library(crosstalk)
@@ -176,7 +235,7 @@ volcano_plots_combined <- subplot(
   volcano_plots,
   nrows = 3,
   shareX = TRUE,
-  shareY = TRUE,
+  shareY = FALSE,
   titleX = FALSE,
   titleY = FALSE
 ) %>%
@@ -191,10 +250,12 @@ volcano_plots_combined <- subplot(
     on = "plotly_hover",
     persistent = FALSE,
     selectize = TRUE,
+    opacityDim = 1,
     selected = attrs_selected(
       opacity = 1,
       marker = list(color = "black", size = 10)
-    )
+    ),
+    debounce = 10
   ) %>%
   toWebGL()
 
@@ -219,16 +280,18 @@ server <- function(input, output, session) {
       gene_symbol = r_selected_gene(),
       counts = input$count_type
     )
-  })
+  }, res = 120)
+  output$plot_out_de <- renderPlot({
+    validate(
+      need(r_selected_gene(), "Click on a point in the volcano plot to select a gene")
+    )
+    plot_de_single_gene_vemu(
+      gene_symbol = r_selected_gene()
+    )
+  }, res = 120)
 }
 
 shinyApp(
   ui, server,
   options = list(port = 4321, launch.browser = FALSE)
 )
-
-
-
-
-
-
